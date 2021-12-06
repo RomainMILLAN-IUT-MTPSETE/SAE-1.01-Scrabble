3d4352f582db0d6a48522b00c331626ca7ad2322
Wabezeter <42139000+Wabez3ter@users.noreply.github.com>
2021-12-06 17:50:23 +0100

    Just change

--- Scrabble.java
+++ Scrabble.java
@@ -1,5 +1,5 @@
 public class Scrabble {
-    private static int[] nbPointsJeton = {1,3,3,4,2,5,2,6,4,1,7,4,3,4,2,7,7,8,9,0,1,3,5,2,7,3};
+    private static int[] nbPointsJeton = {1,3,3,4,2,5,2,6,4,1,7,4,3,4,2,7,7,8,9,3,1,3,5,2,7,3};
 
     
 }
