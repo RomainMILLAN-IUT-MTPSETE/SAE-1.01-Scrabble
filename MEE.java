23ab0b8d1f0f4236b7cb49d6f31850eb6f1241fe
Wabezeter <42139000+Wabez3ter@users.noreply.github.com>
2021-12-06 20:40:35 +0100

    [D-06/12/2021] 20:40

--- MEE.java
+++ MEE.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class MEE {
     //PR => Pré-requis
     //A => Action
@@ -43,6 +45,8 @@ public class MEE {
 
         //Si la variable initialisé avant est true
         if(intinTabPositifOrNull == true){
+            //On initializa le tableau
+            this.tabFreq = new int[tab.length];
             //On fait une boucle pour copier tous les elements de 'tab' dans 'tabFreq'
             for(int i=0; i<tab.length; i++){
                 //On copie les element de tab dans tabFreq
@@ -117,6 +121,7 @@ public class MEE {
         if(i > 0 && i < this.tabFreq.length){
             //Si le chiffre est bon alors on ajoute dans 'tabFreq' 1 exemplaire
             this.tabFreq[i] = this.tabFreq[i]+1;
+            this.nbTotEx++;
         }
     }
 
@@ -132,11 +137,131 @@ public class MEE {
         //Si il existe un exemplaire dans this alors
         if(this.tabFreq[i] > 0){
             this.tabFreq[i] = this.tabFreq[i] - 1;
+            this.nbTotEx--;
             retireFonctionne = true;
         }
 
         return retireFonctionne;
     }
 
+    /**
+     * PR: this est non vide.
+     * A: Retire de this un exemplaire choisi aléatoirement et le retourne.
+     * @return
+     */
+    public int retireAleat(){
+        //Choisi un nombre aléatoire entre 0 et la longueur du tableau -1
+        Random rand = new Random();
+        int indice = rand.nextInt(0 - this.tabFreq.length-1);
+        
+        //Si le nombre placé à l'indice du tableau et > à 0 alors:
+        if(this.tabFreq[indice] > 0){
+            //On retire 1 de cette élément
+            this.tabFreq[indice]--;
+            this.nbTotEx--;
+        //Sinon
+        }else {
+            //On met indice à -1p
+            indice = -1;
+            //On réapelle la fonction
+            retireAleat();
+        }
+        //On retourne l'indice
+        return indice;
+    }
+
+    /**
+     * PR: 0 <= i && i < tabFreq.length
+     * A:Tranfère un exemplaire de i de this vers e s'il en existe, et retourne vrai ssi cette action a pu être effectué.
+     * @param e
+     * @param i
+     * @return
+     */
+    public boolean transfere(MEE e, int i){
+        //On initialize la variable 'resultat', qui est un boolean.
+        boolean resultat = false;
+
+        //On fait les prérequis, si i >= 0 et i est < a la limite de l'ensemble et tabFreq[i] > 0 alors:
+        if(i >= 0 && i < this.tabFreq.length && this.tabFreq[i] > 0){
+            //On retire 1 de this a l'indice donnée.
+            this.retire(i);
+            //On ajoute 1 à l'ensemble e donnée en paramètre et a l'indice donnée.
+            e.ajoute(i);
+            //On passe la variable résultat à true.
+            resultat = true;
+        }
+
+        //On retourne la variable boolean.
+        return resultat;
+    }
+
+    /**
+     * PR: k >= 0
+     * A: transfère k exemplaires choisis aléatoirement de this vers e dans la limite du contenu de this.
+     * @param e
+     * @param k
+     */
+    public void tranfereAleat(MEE e, int k){
+        //Choisi un nombre aléatoire entre 0 et la longueur du tableau -1
+        Random rand = new Random();
+        int indice = rand.nextInt(0 - this.tabFreq.length-1);
+
+        //Si l'élément contenue à l'indicie 'indice' dans le tableau est suppérieur à nombre d'élément que l'on veut enlever alors:
+        if(this.tabFreq[indice] > k){
+            //On fait une boucle pour x fois de i=0 à i<k
+            for(int i=0; i<k; i++){
+                //On retire alors une fois l'élément à l'indice indiquer
+                this.retire(indice);
+            }
+        //Sinon
+        }else {
+            //On affiche une erreur:
+            System.out.println("ERROR: Trop peut d'exemplaire dans l'indice choisis");
+        }
+    }
+
+    /**
+     * PR: tabFreq.length <= v.length
+     * A: Retourne la somme des valeurs des exemplaires des éléments de this, la valeur d'un exemplaire d'un élément i de this étant égale a v[i]
+     * @param v
+     * @return
+     */
+    public int sommeValeurs(int[] v){
+        //On initialize le résulat
+        int resultat = 0;
+
+        //On fait une boucle pour parcourir tous le trableau this
+        for(int i=0; i<this.tabFreq.length; i++){
+            //On ajoute au résultat le nombre d'exemplaire * valeur des points
+            resultat += this.tabFreq[i] * v[i];
+        }
+
+        //Retoune le résultat
+        return resultat;
+    }
+
+
+
+
+    /**
+     * Affiche un Multiple Element Ensemble
+     * @param E
+     */
+    public void afficheMEE() {
+        if(!this.estVide()){
+            String res = "{ ";
+            for (int i=0; i<this.tabFreq.length-1; i++){
+                if(this.tabFreq[i] > 0){
+                    res += tabFreq[i] + ",";
+                }else {
+                    res += "0,";
+                }
+            }
+            res+=" }";
+            System.out.println(res);
+        }else {
+            System.out.println("Error");
+        }
+    }
 
 }
