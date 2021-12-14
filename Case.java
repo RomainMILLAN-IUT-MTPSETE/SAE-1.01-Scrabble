af3bcb4f0b6c46719e8405d0367bbf9b56751260
Wabezeter <42139000+Wabez3ter@users.noreply.github.com>
2021-12-14 09:06:51 +0100

    [D-14/12/2021] Just add class Case and start class Plateau but Plateau is not finish

--- Case.java
+++ Case.java
@@ -1,12 +1,14 @@
 public class Case {
     private int couleur;
+    private char lettre; //0 valeur par défaut
 
     /**
      * PR: uneCouleur est un entier entre 1 et 5
      * A: constructeur de Case
      */
     public Case(int uneCouleur){
-
+        this.couleur = uneCouleur;
+        this.lettre = 0;
     }
 
     /**
@@ -14,7 +16,7 @@ public class Case {
      * @return
      */
     public int getCouleur(){
-
+        return this.couleur;
     }
 
     /**
@@ -22,7 +24,7 @@ public class Case {
      * @return
      */
     public char getLettre(){
-
+        return this.lettre;
     }
 
     /**
@@ -30,18 +32,30 @@ public class Case {
      * @param let
      */
     public void setLettre(char let){
-
+        this.lettre = let;
     }
 
     /**
      * R: vrai si est seuelemnt si la case est recourverte par une lettre
      */
     public boolean estRecouverte(){
+        boolean resultat = false;
+        if(this.lettre != 0){
+            resultat = true;
+        }
 
+        return resultat;
     }
 
     public String toString(){
-        
+        String resultat = "";
+        if(this.estRecouverte() == true){
+            resultat = "La case est recouverte par la lettre " + this.getLettre();
+        }else {
+            resultat = "La case est vide";
+        }
+
+        return resultat;
     }
     
 }
